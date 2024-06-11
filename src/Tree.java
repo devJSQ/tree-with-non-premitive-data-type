c5efcbad88a37d03804eec019754924b9d12d2c6
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-06-12 01:43:53 +0300

    Update Tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -31,9 +31,70 @@ class Tree {
         root = null;
     }
 
+    public void printTree(){
+        if (root == null){
+            System.out.println("empty");
+            return;
+        }
 
-    // 1 - Write a function that prints all tree values
-    /* Your code here  */ 
+        Stack <Node> stack = new Stack<Node>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node cNode = stack.pop();
+
+            System.out.println(cNode.name);
+
+            if(cNode.right != null){
+                stack.push(cNode.right);
+            }
+            if(cNode.left != null){
+                stack.push(cNode.left);
+
+            }
+            if (cNode.center != null) {
+                stack.push(cNode.center);
+            }
+        }
+    }
+
+
+    public boolean searchTree(String e){
+        if (root == null){
+            System.out.println("empty");
+            return false;
+        }
+
+        Stack <Node> stack = new Stack<Node>();
+        stack.push(root);
+
+        boolean found = false;
+
+
+        while (!stack.isEmpty()) {
+            Node cNode = stack.pop();
+
+            if(cNode.name == e){
+                System.out.println(cNode.name);
+                found = true;
+            }
+
+           
+
+            if(cNode.right != null){
+                stack.push(cNode.right);
+            }
+            if(cNode.left != null){
+                stack.push(cNode.left);
+
+            }
+            if (cNode.center != null) {
+                stack.push(cNode.center);
+            }
+        }
+        return found;
+    }
+    
 
    
     // 2 - Write function that search for a pecific name in the tree and returns true if the it's found and return false otherwis 
@@ -42,9 +103,28 @@ class Tree {
     
 
     public static void main(String[] args) {
+        Tree t1 = new Tree(new Node("ahmad"));
+        System.out.println(t1.root.name);
+        t1.root.name = "Anas";
+        t1.root.right = new Node("Sami");
+        t1.root.right.left = new Node("jasser");
+        t1.root.right.left.left = new Node("lina");
+        t1.root.center = new Node("Fatima");
+        t1.root.center.left = new Node("faisal");
+        t1.root.center.right = new Node("rashed");
+        t1.root.left = new Node("Abdullah");
+        t1.root.left.right = new Node("maha");
+        t1.root.left.center = new Node("yasser");
+        t1.root.left.left = new Node("salem");
+
+        t1.printTree();
+        System.out.println(t1.searchTree("rashed"));
+
+        
+        
 /* 
-    3 - Declare Tree
-    4 - Access the root data and print it
+   
+
     5 - Access and update the root data to be (Anas)
     6 - Create root children as the image above shows
     7 - Print tree values
